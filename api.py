14f347ee708470571f004dbea8a517134d910e3e
Jose <31940182+Jalvarado115@users.noreply.github.com>
2020-10-03 12:54:19 -0700

    changed users.py

--- api.py
+++ api.py
@@ -1,36 +1,33 @@
-rom flask import Flask, request, jsonify
-import sqlite
+import flask
+from flask import request, jsonify, g, current_app
+import sqlite3 
+from werkzeug.security import generate_password_hash, check_password_hash
 import logging
 
-app = Flask(__name__)
-app.config.from_envvar('APP_CONFIG')
 
 
-def make_dicts(cursor, row):
-    return dict((cursor.description[idx][0], value)
-                for idx, value in enumerate(row))
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(app.config['DATABASE'])
-        db.row_factory = make_dicts
+        db.row_factory = dict_factory
     return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
-
 
 @app.cli.command('init')
 def init_db():
@@ -39,7 +36,23 @@ def init_db():
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-        
+   
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>MicroBlogging</h1>
+<p>A microblogging service similar to twitter.</p>'''
+
+
+@app.route('/users/all', methods=['GET'])
+def api_all():
+    conn = sqlite3.connect('data.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_books = cur.execute('SELECT * FROM users;').fetchall()
+
+    return jsonify(all_books)
+
 
 #User registration
 
@@ -54,7 +67,7 @@ def createUser():
     
     app.logger.info(username)
     
-    db= get_db()
+    db = get_db()
 
     db.execute('INSERT INTO users (username, email, password) VALUES(?,?,?)',(username, email, hashed_password))
     res = db.commit()
@@ -63,8 +76,6 @@ def createUser():
     return jsonify(getusers)
 
 
-#Authenticating user
-
 @app.route('/login', methods=['POST'])
 def authenticateUser():
     query_parameters = request.form
@@ -72,39 +83,20 @@ def authenticateUser():
     username = query_parameters.get('username')
     password = query_parameters.get('password')
     
-    db= get_db()
+    db = get_db()
     result = query_db('SELECT password FROM users WHERE username = ?', [username])
     
     hashed_password = result[0].get('password')
     return jsonify(check_password_hash(hashed_password, password)) #checking if user entered password is equal to the hashed password in db
-    
-    
-# Allowing user to follow another user
+  
 
-@app.route('/follow', methods=['POST'])
-def addFollower():
-    query_parameters = request.form
-    
-    username = query_parameters.get('username')
-    usernameToFollow = query_parameters.get('usernameToFollow')
-    
-    db= get_db()
-    db.execute('INSERT INTO user_following (username, usernameToFollow) VALUES(?,?)',(username, usernameToFollow))
-    res = db.commit()
-    
-    message = "200 Ok"
-    return jsonify(message)
 
-@app.route('/unfollow', methods=['POST'])
-def removeFollower():
-    query_parameters = request.form
-       
-    username = query_parameters.get('username')
-    usernameToRemove = query_parameters.get('usernameToRemove')
-    
-    db= get_db()
-    db.execute('DELETE FROM user_following WHERE username=? AND usernameToFollow=?',(username, usernameToRemove))
-    res = db.commit()
-    
-    message = "200 Ok"
-    return jsonify(message)
+@app.errorhandler(404)
+def page_not_found(e):
+    return '''<h1>404</h1>
+<p>The resource could not be found.</p>''', 404
+
+app.run()
+
+
+
